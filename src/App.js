340a0c234a6957cd7143da2b91b703389e607381
Martin Sesma <sesmamartin@gmail.com>
2021-12-23 10:57:22 -0300

    Se ordeno parte de la informacion.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {FaLinkedin,  FaMobileAlt, FaHome, FaGithub, FaEnvelope} from 'react-icons/fa';
 
-function App() {
+import './App.css';
+import photo from './assets/photo-cv.jpg';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          Skills
+        </div>
+        <div className='col'>
+          Experiencia
+        </div>
+      </div>
     </div>
-  );
+
+      );
 }
 
 export default App;
